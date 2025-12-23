2de41d9fee94443923572834fc6979bb3fd6f094
Veeran Puthumkara <vkuttyp@gmail.com>
2025-12-23 12:13:54 +0300

    fix: add landing page route for index.md

---
+++ layer/app/pages/[[lang]]/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { ContentNavigationItem, Collections } from '@nuxt/content'
+import { addPrerenderPath } from '../../utils/prerender'
+
+definePageMeta({
+  layout: 'default',
+})
+
+const route = useRoute()
+const { locale, isEnabled } = useDocusI18n()
+
+const collectionName = computed(() => isEnabled.value ? `landing_${locale.value}` : 'landing')
+
+const { data: page } = await useAsyncData(route.path, () => queryCollection(collectionName.value as keyof Collections).path(route.path).first())
+
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
+
+addPrerenderPath(route.path)
+</script>
+
+<template>
+  <UPage v-if="page">
+    <ContentRenderer :value="page" />
+  </UPage>
+</template>
